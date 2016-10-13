2890cdaf81e1fe6a851199d4e2005fd3da19e9bc
Ippokratis <ippobour@gmail.com>
2016-10-14 01:12:44 +0300

    A slightly optimized version of keijiro PageFlipping
    A slightly optimized version of keijiro PageFlipping down from 75ms to
    32ms per frame on my slow integrated GPU

---
+++ Assets/PageFlippingAnimation.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class PageFlippingAnimation : MonoBehaviour
+{
+    [SerializeField]
+    Texture _texture;
+    [SerializeField]
+    float _interval = 0.15f;
+    [SerializeField]
+    float _delay = 0.0f;
+
+    MaterialPropertyBlock _props;
+    Vector3 _position;
+    public float _lastTime;
+    Renderer _renderer;
+    static int _Progress;
+    static int _PreviousProgress;
+    Transform _transform;
+
+    void Start()
+    {
+        _props = new MaterialPropertyBlock();
+        _props.SetTexture("_MainTex", _texture);
+        _position = transform.position;
+        //Cache
+        _transform = GetComponent<Transform>();
+        _renderer = GetComponent<Renderer>();
+        _Progress = Shader.PropertyToID("_Progress");
+        _PreviousProgress = Shader.PropertyToID("_PreviousProgress");
+
+    }
+
+    void Update()
+    {
+        var time = ((Time.time - _delay) / _interval) % 2;
+
+        _props.SetFloat(_Progress, time);
+        _props.SetFloat(_PreviousProgress, _lastTime);
+        _renderer.SetPropertyBlock(_props);
+        _transform.position = _position + Vector3.forward * time * 0.01f;
+
+        _lastTime = time;
+    }
+}
---
+++ Assets/TextureSwapper.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class TextureSwapper : MonoBehaviour
+{
+    [SerializeField] RenderTexture[] _textures;
+    [SerializeField] float _interval = 0.1f;
+    Camera cam;
+
+    void Start()
+    {
+        Application.targetFrameRate = 90;
+        //Cache
+        cam = GetComponent<Camera>();
+    }
+
+    void Update()
+    {
+        cam.targetTexture =
+            _textures[(int)(Time.time / _interval) % _textures.Length];
+    }
+}
